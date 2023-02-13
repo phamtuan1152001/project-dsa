f170b7bbdafa17b751293e79b25448e2785eeb3f
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-02-13 19:43:59 +0700

    install scss

--- src/Modules/StartGame/index.js
+++ src/Modules/StartGame/index.js
@@ -1,8 +1,10 @@
+import "./index.scss";
+
 import React from "react";
 
 const StartGame = () => {
   return (
-    <div>
+    <div className="start-game-wrapper">
       <h1>StartGame page</h1>
     </div>
   );
